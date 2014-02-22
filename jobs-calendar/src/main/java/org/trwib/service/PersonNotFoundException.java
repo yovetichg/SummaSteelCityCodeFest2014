fc0ebf4e8d20e667742cce7a41575cdd422994d1
Brian Gray <bgray@summa-tech.com>
2014-02-22 09:22:22 -0500

    Generated Maven archetype, Maven/Spring/JPA/Knockout stack

---
+++ jobs-calendar/src/main/java/org/trwib/service/PersonNotFoundException.java
@@ -0,0 +1,8 @@
+package org.trwib.service;
+
+/**
+ * This exception is thrown if the wanted person is not found.
+ * @author Petri Kainulainen
+ */
+public class PersonNotFoundException extends Exception {
+}
---
+++ jobs-calendar/src/main/webapp/static/js/app.js
@@ -0,0 +1,18 @@
+/**
+ * Application JS
+ * @param first
+ * @param last
+ * @constructor
+ */
+var ViewModel = function(first, last) {
+  this.firstName = ko.observable(first);
+  this.lastName = ko.observable(last);
+
+  this.fullName = ko.computed(function() {
+    // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because
+    // these get called when evaluating fullName.
+    return this.firstName() + " " + this.lastName();
+  }, this);
+};
+
+ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
