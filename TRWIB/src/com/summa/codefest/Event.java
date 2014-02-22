c4f25b8514441ebeb6925034db6d2a03392feae1
mitch <mitch@summa-tech.com>
2014-02-22 14:42:38 -0500

    New Server Project

---
+++ TRWIB/src/com/summa/codefest/Event.java
@@ -0,0 +1,84 @@
+package com.summa.codefest;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the events database table.
+ * 
+ */
+@Entity
+@Table(name="events")
+@NamedQuery(name="Event.findAll", query="SELECT e FROM Event e")
+public class Event implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	private String description;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date end;
+
+	private String location;
+
+	private String name;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date start;
+
+	public Event() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getEnd() {
+		return this.end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public String getLocation() {
+		return this.location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStart() {
+		return this.start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+}
---
+++ TRWIB/src/com/summa/codefest/RestController.java
@@ -0,0 +1,76 @@
+package com.summa.codefest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Controller
+public class RestController {
+	
+	@RequestMapping(value = "/getusers", method = RequestMethod.GET, produces="application/json")
+	public @ResponseBody String getUsers() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+		
+		User user1 = new User();
+		user1.setId(1000);
+		user1.setIP("1.1.1.1");
+
+		User user2 = new User();
+		user2.setId(2000);
+		user2.setIP("2.2.2.2");
+		
+		List<User> list = new ArrayList<User>();
+		list.add(user1);
+		list.add(user2);
+
+		ObjectWriter ow = objectMapper.writer();
+		ow.with(SerializationFeature.WRAP_ROOT_VALUE);
+		String str = null;
+		try {
+			str = ow.writeValueAsString(list);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println("User JSON is\n" + str);
+		return str.toString();
+	}
+
+	@RequestMapping(value = "/getevents", method = RequestMethod.GET, produces="application/json")
+	public @ResponseBody String getEvents() {
+		DAO dao = new DAO();
+		EntityManager em = dao.getEntityManager();
+		
+		TypedQuery<Event> query = em.createQuery("SELECT e FROM Event e", Event.class);
+		List<Event> events = query.getResultList();
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+		
+		ObjectWriter ow = objectMapper.writer();
+		ow.with(SerializationFeature.WRAP_ROOT_VALUE);
+		String str = null;
+		try {
+			str = ow.writeValueAsString(events);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		em.close();
+		return str.toString();
+	}
+}
