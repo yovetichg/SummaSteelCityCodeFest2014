56600ac8cab41cd1c60448b4831b078ac42be39b
Rob Angevine <rob@summa-tech.com>
2014-02-23 00:35:32 -0500

    no message

--- jobs-calendar/src/main/webapp/static/js/app/vm.js
+++ jobs-calendar/src/main/webapp/static/js/app/vm.js
@@ -3,23 +3,41 @@
 var testeventsVM = {
         page: ko.observable("0"),
         size: ko.observable("10"),
-       events: ko.observable("0.0.0.0"),
+        events: ko.observable("0.0.0.0"),
         server_id2: ko.observable("0.0.0.0"),
-        
         node_port: ko.observable("13390")
     };
 
-var questionsVM = ko.observableArray([]);
+var questionIDVM = ko.observable("2");
+
+var questionAnswerVM = ko.observable("no");
+
+var questionVM =  ko.observable("Need a question");
+var questionIDVM = ko.observable("0");
+
+var categoryVM = ko.observable("Needs");
+var categoryIDVM = ko.observable("19");
 
 var eventsVM = ko.observableArray([]);
 
+var eventFilterVM = {
+        categoryIDFilter: ko.observable("0")
+    };
+
 var eventslistTestVM = ko.observableArray([
-    
     {id: ko.observable("98"),
-    name:ko.observable("Recruiting for Pickers Packers"),
-    startDate:("2014-02-22"),
-    endDate:("2014-02-22"),
-    location:("PA Careerlink Downtown Pittsburgh")},
+    name: ko.observable("Recruiting for Pickers Packers"),
+    startDate: ko.observable("2014-02-22"),
+    endDate: ko.observable("2014-02-22"),
+    locationName: ko.observable("PA Careerlink Downtown Pittsburgh"),
+    locationAddr1: ko.observable("9600 Babcock Blvd"),
+    locationAddr2: ko.observable(""),
+    locationCity: ko.observable("Allison Park"),
+    locationState: ko.observable("PA"),
+    locationZipcode: ko.observable("15101"),
+    description: ko.observable("When: Mon Mar 4, 2013 9:30am to 11am&nbsp; EST<br> <br>Where: CareerLink<br>Event Status: confirmed"),
+    categoryID: ko.observable("12") 
+    },
     {id: ko.observable("98"),
     name:ko.observable(" Pickers Packers"),
     startDate:("2014-02-22"),
@@ -30,23 +48,26 @@ var eventslistTestVM = ko.observableArray([
     startDate:("2014-02-22"),
     endDate:("2014-02-22"),
     location:("location 3")},
-    
-    
 ]);
     
 var eventslistVM = ko.observableArray([]);
 
 getevents();
-getquestions();
+getquestions(0,'yes');
     
 
 function viewModel(){
     var self = this;
-
-   
     self.events = eventsVM;
     self.eventsTEST = eventslistTestVM;
-    self.questions = questionsVM;
+    self.question = questionVM;
+    self.questionID = questionIDVM;
+    self.category = categoryVM;
+    self.categoryID = categoryIDVM;
+    self.questionAnswer = questionAnswerVM;
+    self.eventFilter = eventFilterVM;
+    
+    
 	
 	}
 	
@@ -55,17 +76,42 @@ ko.applyBindings(new viewModel());
 var vm = new viewModel();
     
 
-function getquestions(question) {
+function getquestions(id,answer) {
+    if (id == 0){
+            urlconcat = "http://10.93.126.85:8080/jobs-calendar/api/question";
+            }
+            else {
+            urlconcat = "http://10.93.126.85:8080/jobs-calendar/api/question/" + id + "/" + answer;            
+            };
+    
     var request = $.ajax({
-            url:"http://10.93.126.85:8080/TRWIB/restful/getquestions",
+        
+            
+                         
+            url:urlconcat,
             type: 'GET',
             dataType:'jsonp',
-            data: question,
-            jsonpCallback: 'questions',
-            jsonp: 'wrapper',
+            jsonpCallback: 'question',
+            jsonp: 'callback',
             success: function (data) {
             console.log("question call good");
-            console.log(data);
+            
+             if (!data.question) {
+                 console.log("is category");
+                 console.log(data.category);
+                 categoryVM(data.category.category);
+                 categoryIDVM(data.category.id);
+                 getevents(data.category.id);
+             }
+            else {
+              console.log("is question");    
+              questionVM(data.question.question);
+              questionIDVM(data.question.id);
+            };
+                
+                  
+            
+            
             
             },
             error: function (request, status, error) {
@@ -74,23 +120,29 @@ function getquestions(question) {
         });
     }
 
-    function getevents() {
+function getevents(catID) {
+    if (catID == 19){
+            urlconcat = "http://10.93.126.85:8080/jobs-calendar/api/events/19";
+            }
+            else {
+            urlconcat = "http://10.93.126.85:8080/jobs-calendar/api/events/" + catID ;            
+            };
     var request = $.ajax({
-            url:"http://10.93.126.85:8080/TRWIB/restful/getevents",
+            url:urlconcat,
             type: 'GET',
             dataType:'jsonp',
             jsonpCallback: 'events',
-            jsonp: 'wrapper',
+            jsonp: 'callback',
             success: function (data) {
             console.log("event call good");
-            console.log(data);
-             $.each(data, function (i, item) {
-                                
-                            
-                                eventsVM.push(item);
-                             
-                                eventsVM.valueHasMutated();
-             });
+            
+            
+                eventsVM.removeAll();
+                $.each(data, function (i, item) {         
+                    eventsVM.push(item);             
+                    eventsVM.valueHasMutated();            
+                });
+            
             
             },
             error: function (request, status, error) {
@@ -98,6 +150,15 @@ function getquestions(question) {
             }
         });
     }
+
+   
+function answerQuestion(id,answer) {
+    
+    console.log(answer);
+    getquestions(id,answer);
+    
+}
+
 
     
     
