97547bf15bce0137d8d4038c4ddab301feb14c1d
mitch <mitch@summa-tech.com>
2014-02-22 21:34:53 -0500

    Fixed JSONP on Question

--- jobs-calendar/src/main/java/org/trwib/controller/QuestionResponseController.java
+++ jobs-calendar/src/main/java/org/trwib/controller/QuestionResponseController.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.trwib.dto.QuestionResponseDto;
 import org.trwib.model.QuestionResponse;
 import org.trwib.service.QuestionResponseService;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 import javax.annotation.Resource;
 
@@ -29,24 +32,25 @@ public class QuestionResponseController {
      * Gets the first question
      */
     @ResponseBody
-    @RequestMapping(value = "/question", method = RequestMethod.GET)
-    public QuestionResponseDto firstQuestion() {
+    @RequestMapping(value = "/question", method = RequestMethod.GET, produces="application/json")
+    public JSONPObject firstQuestion(@RequestParam("callback") final String callback) {
         LOGGER.debug("Getting first question");
         QuestionResponse first = questionResponseService.getFirst();
-        return new QuestionResponseDto(first);
+        return new JSONPObject(callback, new QuestionResponseDto(first));
     }
 
     /**
      * Gets the first question
      */
     @ResponseBody
-    @RequestMapping(value = "/question/{questionId}/{yesNo}", method = RequestMethod.GET)
-    public QuestionResponseDto nextQuestion(@PathVariable("questionId") Long questionId,
-                                         @PathVariable("yesNo") String yesNo) {
+    @RequestMapping(value = "/question/{questionId}/{yesNo}", method = RequestMethod.GET, produces="application/json")
+    public JSONPObject nextQuestion(@PathVariable("questionId") Long questionId,
+                                         @PathVariable("yesNo") String yesNo,
+                                         @RequestParam("callback") final String callback) {
         LOGGER.debug("Getting next question: [{}] for answer [{}]", questionId, yesNo);
         boolean yesResponse = "yes".equalsIgnoreCase(yesNo);
         QuestionResponse next = questionResponseService.getNext(questionId, yesResponse);
-        return new QuestionResponseDto(next);
+        return new JSONPObject(callback, new QuestionResponseDto(next));
     }
 
 }
